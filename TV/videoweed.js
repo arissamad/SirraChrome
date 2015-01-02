632f550c9548ec1839f36b3c916c4d674b036dea
Aris Samad-Yahaya <aris@quickschools.com>
2015-01-01 21:24:38 -0800

    Kickass - Show wiki and imdb links

--- TV/videoweed.js
+++ TV/videoweed.js
@@ -3,6 +3,7 @@ console.log("Activated plugin! Auto-close videoweed ads.");
 function auto_open() {
 	$($("#playerpageads_table td")[1]).remove();
 	$(".ad").remove();
+	$($.find("#playerpage > div")).first().remove();
 }
 
 setTimeout(auto_open, 200);
