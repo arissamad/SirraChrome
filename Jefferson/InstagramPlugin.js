a786fb689aaa2d29a9d4fb99f6b3108f1019fee3
Aris Samad <aris@quickschools.com>
2017-02-11 19:44:21 -0800

    First working version

--- Jefferson/InstagramPlugin.js
+++ Jefferson/InstagramPlugin.js
@@ -3,9 +3,46 @@ setInterval(function() {
 	plugin.checkForVideo()
 }, 1000)
 
+setTimeout(function() {
+	if(localStorage.getItem("jefferson-compilations") == null) {
+		plugin.loadCompilations();
+	} else {
+		plugin.renderCompilationSelector();
+	}
+}, 1000)
+
 function InstagramPlugin() {
 	this.foundVideo = false;
 }
+
+InstagramPlugin.prototype.loadCompilations = function() {
+	$.get("https://sirra-jefferson.herokuapp.com/api/compilations/getactive", function(compilationsStr) {
+		localStorage.setItem("jefferson-compilations", compilationsStr);
+		
+		if(this.compilationSelector != null) {
+			this.compilationSelector.remove();
+		}
+		
+		this.renderCompilationSelector();
+	}.bind(this));
+};
+
+InstagramPlugin.prototype.renderCompilationSelector = function() {
+	compilations = JSON.parse(localStorage.getItem("jefferson-compilations"));
+	currentId = localStorage.getItem("jefferson-currentId");
+	
+	console.log("Compilations is" + compilations);
+	console.log("Current ID is ", currentId);
+	
+	this.compilationSelector = this.makeSelect(compilations, "id", "name");
+	this.compilationSelector.change(function(e) {
+		localStorage.setItem("jefferson-currentId", this.compilationSelector.val());
+	}.bind(this));
+	
+	this.compilationSelector.val(currentId);
+	
+	$("body").append(this.compilationSelector);
+};
 
 InstagramPlugin.prototype.checkForVideo = function() {
 	var video = $("video")
@@ -29,13 +66,17 @@ InstagramPlugin.prototype.checkForVideo = function() {
 		newDiv.append(this.makeButton("Set Start", this.clickedStart));
 		newDiv.append(this.makeButton("Set End", this.clickedEnd));
 		newDiv.append(this.makeButton("Preview", this.clickedPreview));
-		newDiv.append(this.makeButton("Save", this.clickedSave));
+		newDiv.append(this.makeButton("Refresh Compilations", this.loadCompilations));
+		newDiv.append(this.makeButton("Grab Video", this.clickedGrabVideo));
 		
 		this.startTimeText = this.makeText("Start time: ");
 		newDiv.append(this.startTimeText);
 		
 		this.endTimeText = this.makeText("End time: ");
 		newDiv.append(this.endTimeText);
+		
+		this.progressText = this.makeText("Progress: ");
+		newDiv.append(this.progressText);
 		
 		parent.append(newDiv);
 	}
@@ -62,8 +103,80 @@ InstagramPlugin.prototype.clickedPreview = function() {
 	this.videoEl.play();
 };
 
-InstagramPlugin.prototype.clickedSave = function() {
+InstagramPlugin.prototype.clickedGrabVideo = function() {
+	var url = $(this.videoEl).attr("src");
+	console.log("Grabbing ", url);
 	
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.responseType = "blob";
+	
+	request.addEventListener("progress", function(e) {
+		if (e.lengthComputable) {
+			var percentComplete = _.round(100 * e.loaded / e.total) + "%";
+		} else {
+			var percentComplete = e.loaded + " bytes";
+		}
+		this.updateProgress("Downloading video", percentComplete);
+	}.bind(this));
+
+	request.onload = function(e) {
+	  var blob = request.response;
+	  this.prepareToTransmit(blob);
+	}.bind(this);
+	
+	request.send(null);
+};
+
+InstagramPlugin.prototype.prepareToTransmit = function(blob) {
+	this.progressText.setValue("Saving video information...");
+	
+	$.get("https://sirra-jefferson.herokuapp.com/api/videos/updatevideo", {
+		name: document.title,
+		url: location.href,
+		startTimeStr: this.startTime,
+		endTimeStr: this.endTime,
+		compilationId: this.compilationSelector.val()
+	}, function() {
+		this.progressText.setValue("Saved!");
+		this.transmitVideoFile(blob);
+	}.bind(this));
+};
+
+InstagramPlugin.prototype.transmitVideoFile = function(blob) {
+	var url = "https://sirra-jefferson.herokuapp.com/upload/uploadvideo/1";
+
+	var request = new XMLHttpRequest();
+	request.open("POST", url, true);
+	request.onload = function (e) {
+	  console.log("Transmitted video file");
+	};
+	
+	request.upload.addEventListener("progress", function(e) {
+		if (e.lengthComputable) {
+			var percentComplete = _.round(100 * e.loaded / e.total) + "%";
+		} else {
+			var percentComplete = e.loaded + " bytes";
+		}
+		this.updateProgress("Uploading video", percentComplete);
+	}.bind(this));
+
+	request.send(blob);
+};
+
+InstagramPlugin.prototype.updateProgress = function(type, progress) {
+	this.progressText.setValue(type + " - " + progress + "");
+};
+
+InstagramPlugin.prototype.clickedSave = function() {
+	$.get("https://sirra-jefferson.herokuapp.com/api/videos/updatevideo", {
+		name: document.title,
+		url: location.href,
+		startTimeStr: this.startTime,
+		endTimeStr: this.endTime
+	}, function() {
+		this.progressText.setValue("Saved!");
+	}.bind(this));
 };
 
 InstagramPlugin.prototype.checkTime = function() {
@@ -86,7 +199,7 @@ InstagramPlugin.prototype.makeStyleBlock = function() {
 	str += ".jefferson-button {";
 	str += "display: inline-block;";
 	str += "text-align: center;";
-	str += "padding: 2px 20px;";
+	str += "padding: 2px 10px;";
 	str += "border: 1px solid #3897f0;";
 	str += "color: #3897f0;";
 	str += "border-radius: 3px;";
@@ -121,4 +234,27 @@ InstagramPlugin.prototype.makeText = function(label) {
 		div.find('.jefferson-value').text(text);
 	}
 	return div;
+};
+
+InstagramPlugin.prototype.makeDownloadLink = function() {
+	var link = $("<a>Download Video</a>");
+	var src = $(this.videoEl).attr("src");
+	link.attr("download", "download");
+	link.attr("href", src);
+	return link;
+};
+
+InstagramPlugin.prototype.makeSelect = function(options, idAttribute, displayAttribute) {
+
+	var selectEl = $("<select style=\"position: fixed; top: 0px; left: 0px; z-index:10000;\"></select>");
+	for(var i=0; i<options.length; i++) {
+		var option = options[i];
+		var optionEl = $("<option value='" + option[idAttribute] + "'>" + option[displayAttribute] + "</option>")
+		selectEl.append(optionEl);
+	}
+	selectEl.click(function(e) {
+		console.log("Got click");
+		e.stopPropagation();
+	});
+	return selectEl;
 };
